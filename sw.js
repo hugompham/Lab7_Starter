871e379ff81b6daa4ba355f65bb75086957ea012
hugompham <hmpham@ucsd.edu>
2022-11-13 14:59:57 -0800

    test

--- sw.js
+++ sw.js
@@ -39,18 +39,16 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+  event.respondWith(caches.open(CACHE_NAME).then(async (cache) => {
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
-  return cache.match(event.request).then((cachedResponse) => {
+    const cachedResponse = await cache.match(event.request);
     if (cachedResponse) {
       return cachedResponse;
     }
-    return fetch(event.request).then((fetchedResponse) => {
-      cache.put(event.request, fetchedResponse.clone());
-      return fetchedResponse;
-    });
-  });
+    const fetchedResponse = await fetch(event.request);
+    cache.put(event.request, fetchedResponse.clone());
+    return fetchedResponse;
   }));
 });
